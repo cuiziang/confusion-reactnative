f11cd05454592fca9339c314a2c2b6d000dcdcb9
Ziang Cui <ziang@gatech.com>
2020-03-06 18:11:13 -0500

    Local Notifications

--- components/ReservationComponent.js
+++ components/ReservationComponent.js
@@ -2,6 +2,8 @@ import React, {Component} from 'react';
 import {Text, View, ScrollView, StyleSheet, Picker, Switch, Button, Modal, Alert} from 'react-native';
 import DatePicker from 'react-native-datepicker'
 import * as Animatable from "react-native-animatable"
+import { Notifications } from 'expo';
+import * as Permissions from 'expo-permissions';
 
 class Reservation extends Component {
 
@@ -38,6 +40,7 @@ class Reservation extends Component {
                 },
                 {
                     text: 'OK', onPress: () => {
+                        this.presentLocalNotification(this.state.date)
                         this.resetForm();
                     }, style: 'ok'
                 }
@@ -52,6 +55,40 @@ class Reservation extends Component {
             smoking: false,
             date: '',
             showModal: false
+        });
+    }
+
+    async obtainNotificationPermission() {
+        let permission = await Permissions.getAsync(
+            Permissions.USER_FACING_NOTIFICATIONS
+        );
+        console.log('THIS IS STATUS FROM NOTIFICATION', permission.status);
+        if (permission.status !== 'granted') {
+            permission = await Permissions.askAsync(
+                Permissions.USER_FACING_NOTIFICATIONS
+            );
+            if (permission.status !== 'granted') {
+                Alert.alert('Permission not granted to show notifications');
+            }
+        }
+        console.log('THIS IS OUR FINAL PERMISSION', permission);
+        return permission;
+    }
+
+    async presentLocalNotification(date) {
+        console.log('PRESENTLOCALNOTIFICATION I AM CALLED FORM MODAL');
+        await this.obtainNotificationPermission();
+        Notifications.presentLocalNotificationAsync({
+            title: 'Your Reservation',
+            body: `Reservation for ${date} requested`,
+            ios: {
+                sound: true,
+            },
+            android: {
+                sound: true,
+                vibrate: true,
+                color: '#512DA8',
+            },
         });
     }
 
