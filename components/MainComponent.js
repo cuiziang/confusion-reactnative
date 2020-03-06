d4041a10fde3139a266b160c7bd7f22d50d0b8fc
Ziang Cui <ziang@gatech.com>
2020-03-05 23:19:40 -0500

    Favorites

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -11,6 +11,7 @@ import {Icon} from "react-native-elements"
 import {connect} from "react-redux"
 import {fetchComments, fetchDishes, fetchLeaders, fetchPromos} from "../redux/ActionCreators"
 import Reservation from "./ReservationComponent"
+import Favorites from "./FavoriteComponent"
 
 const mapStateToProps = state => {
     return {
@@ -107,6 +108,23 @@ const ContactUsNavigator = createStackNavigator({
 
 const ReservationNavigator = createStackNavigator({
     Reservation: {screen: Reservation}
+}, {
+    navigationOptions: ({navigation}) => ({
+        headerStyle: {
+            backgroundColor: "#512DA8"
+        },
+        headerTitleStyle: {
+            color: "#fff"
+        },
+        headerTintColor: "#fff",
+        headerLeft: <Icon name="menu" size={24}
+                          iconStyle={{color: 'white'}}
+                          onPress={() => navigation.toggleDrawer()}/>
+    })
+})
+
+const FavoritesNavigator = createStackNavigator({
+    Favorites: {screen: Favorites}
 }, {
     navigationOptions: ({navigation}) => ({
         headerStyle: {
@@ -236,6 +254,22 @@ const MainNavigator = createDrawerNavigator({
                 drawerIcon: ({tintColor, focused}) => (
                     <Icon
                         name='cutlery'
+                        type='font-awesome'
+                        size={24}
+                        iconStyle={{color: tintColor}}
+                    />
+                ),
+            }
+        },
+    Favorites:
+        {
+            screen: FavoritesNavigator,
+            navigationOptions: {
+                title: 'My Favorites',
+                drawerLabel: 'My Favorites',
+                drawerIcon: ({tintColor, focused}) => (
+                    <Icon
+                        name='heart'
                         type='font-awesome'
                         size={24}
                         iconStyle={{color: tintColor}}
